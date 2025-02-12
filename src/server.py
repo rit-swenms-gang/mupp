e1f33b99638b2827185b3561c6d8083514b7b06e
Christian Ashley <christianzashley@gmail.com>
2025-02-12 13:12:20 -0500

    Update server for CORS with client

--- src/server.py
+++ src/server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 class Root(Resource):
   def get(self):
     return 'Hello world'
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Root, '/')
