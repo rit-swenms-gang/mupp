56b066af75cc9678b6fb025fca4667201af93be1
Christian Ashley <christianzashley@gmail.com>
2025-02-12 13:12:45 -0500

    Set up communication with python server on 5001

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [serverText, setServerText] = useState('yet to access server');
+
+  useEffect(() => {
+    const ac = new AbortController()
+    setServerText('Calling server')
+
+    fetch('http://localhost:5001', {
+      signal: ac.signal
+    })
+      .then(res => {
+        if (!res.ok) return 'Failed to get response'
+        return res.json() 
+      })
+      .then(text => setServerText(text))
+      .catch(e => {
+        console.error(e)
+        setServerText('Something broke')
+      })
+
+    return () => ac.abort()
+  }, [])
 
   return (
     <>
@@ -23,6 +44,9 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <p>
+          Communication with port <code>5001</code> server: {serverText}
         </p>
       </div>
       <p className="read-the-docs">
