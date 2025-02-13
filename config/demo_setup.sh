743ddfdee0c3ceb64bdc024cfb71eb636d238bbf
Christian Ashley <christianzashley@gmail.com>
2025-02-13 15:14:00 -0500

    Correct script for postgres user

--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,5 +1,5 @@
-createdb -h localhost -U cashley mupp
-psql -d mupp -U cashley -f config/demo_db_setup.sql
+createdb -h localhost -U postgres mupp
+psql -d mupp -U postgres -f config/demo_db_setup.sql
 echo "database: mupp
 host: localhost
 user: postgres
