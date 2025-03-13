27738317c060e8fb568e08d5f382258baba73097
Christian Ashley <christianzashley@gmail.com>
2025-03-13 17:35:48 -0400

    Move CI setup to separate script

---
+++ config/ci_setup.sh
@@ -0,0 +1,7 @@
+echo "Testing env vars: $POSTGRES_HOST"
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
+echo "database: mupp
+host: $POSTGRES_HOST
+user: $POSTGRES_USER
+password: $POSTGRES_PASSWORD
+port: $POSTGRES_PORT" >> config/db.yml
--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,7 +1,6 @@
-echo "Testing env vars: $POSTGRES_HOST"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
+psql -d mupp -U postgres -f config/demo_db_setup.sql
 echo "database: mupp
-host: $POSTGRES_HOST
-user: $POSTGRES_USER
-password: $POSTGRES_PASSWORD
-port: $POSTGRES_PORT" >> config/db.yml
+host: localhost
+user: postgres
+password: does_not_matter
+port: 5432" >> config/db.yml
