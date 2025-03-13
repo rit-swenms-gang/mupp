6343fadc3b6ba1aaf6fff0e5debf06394376093f
Christian Ashley <christianzashley@gmail.com>
2025-03-13 16:11:32 -0400

    Add DB name to postgres service

--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,5 +1,4 @@
 echo "Testing env vars: $POSTGRES_HOST"
-PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -f config/mupp_setup.sql
 PGPASSWORD=$POSTGRES_PASSWORD psql -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
 echo "database: mupp
 host: $POSTGRES_HOST
