8d75299ee4fefebb902cd1b8cdb0f1a5377d38fb
Christian Ashley <christianzashley@gmail.com>
2025-03-13 15:03:48 -0400

    Manually supply PGPASSWORD to psql commands

--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,6 +1,6 @@
 echo "Testing env vars: $POSTGRES_HOST"
-createdb -h $POSTGRES_HOST -U $POSTGRES_USER --password $POSTGRES_PASSWORD mupp
-psql -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
+PGPASSWORD=$POSTGRES_PASSWORD createdb -h $POSTGRES_HOST -U $POSTGRES_USER -w mupp
+PGPASSWORD=$POSTGRES_PASSWORD psql -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
 echo "database: mupp
 host: $POSTGRES_HOST
 user: $POSTGRES_USER
