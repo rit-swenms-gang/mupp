598b8d8bdfc43969c13a6c58896402bbbf989321
Christian Ashley <christianzashley@gmail.com>
2025-03-13 14:53:59 -0400

    Manually include password in createdb

--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,8 +1,8 @@
 echo "Testing env vars: $POSTGRES_HOST"
-createdb -h $POSTGRES_HOST -U $POSTGRES_USER -w mupp
+createdb -h $POSTGRES_HOST -U $POSTGRES_USER --password $POSTGRES_PASSWORD mupp
 psql -d mupp -U $POSTGRES_USER -f config/demo_db_setup.sql
 echo "database: mupp
 host: $POSTGRES_HOST
 user: $POSTGRES_USER
 password: $POSTGRES_PASSWORD
-port: 5432" >> config/db.yml
+port: $POSTGRES_PORT" >> config/db.yml
