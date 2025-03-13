d0a248d8b411a74891a0d827ae224c5e0a2b2b39
Christian Ashley <christianzashley@gmail.com>
2025-03-13 11:02:06 -0400

    Add POSTGRES_PASSWORD env var to run db setup

--- config/demo_setup.sh
+++ config/demo_setup.sh
@@ -1,7 +1,7 @@
-createdb -h localhost -U postgres mupp
+createdb -h localhost -U postgres -w mupp
 psql -d mupp -U postgres -f config/demo_db_setup.sql
 echo "database: mupp
 host: localhost
 user: postgres
-password: does_not_matter
+password: $POSTGRES_PASSWORD
 port: 5432" >> config/db.yml
