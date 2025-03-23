11144aed2041dd0c7a568b991af5a9c8dcd64963
Christian Ashley <christianzashley@gmail.com>
2025-03-23 15:23:55 -0400

    Add initial tests for Table.insert

--- tests/db/utils/test_table_utils.py
+++ tests/db/utils/test_table_utils.py
@@ -22,6 +22,7 @@ class TableUtilsTest(TestCase):
       """
     )
     self.db.fetch_tables()
+    self.table = self.db.tables[self.table_name]
 
   def tearDown(self):
     self.db.cleanup(True)
@@ -29,21 +30,29 @@ class TableUtilsTest(TestCase):
   def test_table_is_fetched(self):
     self.assertIsNotNone(self.db.tables)
     self.assertEqual(1, len(self.db.tables), 'Expected single Table in test schema')
-    self.assertEqual(self.table_name, str(self.db.tables[self.table_name]), "Expected table to have name {}".format(self.table_name))
+    self.assertEqual(self.table_name, str(self.table), "Expected table to have name {}".format(self.table_name))
 
   def test_select_retrieves_all_table_entities(self):
-    table = self.db.tables[self.table_name]
-    res = table.select()
+    res = self.table.select()
     self.assertIsNotNone(res, 'Expected response from database')
     self.assertEqual(2, len(res), 'Expected array of 2 entities from database')
 
   def test_select_retrieves_entity_by_WHERE(self):
-    table = self.db.tables[self.table_name]
-    res1 = table.select(where={ 'id': 1 })
+    res1 = self.table.select(where={ 'id': 1 })
     self.assertEqual({ 'id': 1, 'test_field': 'dummy' }, res1[0], 'Expected to retrieve object by id')
-    res2 = table.select(where={ 'test_field': 'another dummy' })
+    res2 = self.table.select(where={ 'test_field': 'another dummy' })
     self.assertEqual({ 'id': 2, 'test_field': 'another dummy' }, res2[0], 'Expected to retrieve object by test_field')
 
-  # def test_insert(self):
-    
+  def test_insert_adds_entity(self):
+    init_cout = self.db.exec_commit("SELECT COUNT(*) FROM {};".format(self.table_name))[0]
+    self.table.insert({ 'test_field': 'inserted' })
+    updated_count = self.db.exec_commit("SELECT COUNT(*) FROM {};".format(self.table_name))[0]
+    self.assertEqual(init_cout + 1, updated_count, 'Expected count to increase by 1')
+    inserted = self.db.exec_commit("SELECT * FROM {} WHERE test_field = %s;".format(self.table_name), ['inserted'])
+    self.assertEqual((updated_count, 'inserted'), inserted, 'Expected new entity to be found')
+
+  def test_insert_adds_entity_and_returns(self):
+    res = self.table.insert({ 'test_field': 'inserted' }, ['id', 'test_field'])
+    updated_count = self.db.exec_commit("SELECT COUNT(*) FROM {};".format(self.table_name))[0]
+    self.assertEqual((updated_count, 'inserted'), res, 'Expected new entity to returned in method call')
   
