8f10f5046c341816f9d22e3cced97a06189806cc
Christian Ashley <christianzashley@gmail.com>
2025-04-05 12:14:29 -0400

    Add initial hosted_forms table schema to db setup

--- config/demo_db_setup.sql
+++ config/demo_db_setup.sql
@@ -1,18 +1,18 @@
 BEGIN;
 
-DROP TABLE IF EXISTS accounts CASCADE;
-CREATE TABLE accounts(
-  id SERIAL,
-  username VARCHAR,
-  email VARCHAR UNIQUE NOT NULL,
-  password VARCHAR NOT NULL,
-  salt VARCHAR NOT NULL
-);
-
 DROP TABLE IF EXISTS mupp_setup_demo CASCADE;
 CREATE TABLE mupp_setup_demo(
   id SERIAL,
   name VARCHAR
+);
+
+DROP TABLE IF EXISTS accounts CASCADE;
+CREATE TABLE accounts(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR,
+  email VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL,
+  salt VARCHAR NOT NULL
 );
 
 DROP TABLE IF EXISTS logins CASCADE;
@@ -22,7 +22,14 @@ CREATE TABLE logins(
   session_key VARCHAR UNIQUE NOT NULL
 );
 
+DROP TABLE IF EXISTS hosted_forms;
+CREATE TABLE hosted_forms(
+  id UUID DEFAULT gen_random_uuid(),
+  account_id INT NOT NULL REFERENCES accounts(id),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  expires_at TIMESTAMP -- NOT NULL
+);
+
 INSERT INTO mupp_setup_demo (name) VALUES ('test1'), ('test2'), ('test3');
--- INSERT INTO accounts (name, password) VALUES ('test', 'dummy'), ('acount1', 'password');
 
 COMMIT;
