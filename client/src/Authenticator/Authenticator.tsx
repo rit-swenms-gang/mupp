4c9b2ee3e10cc4acccd01029e3756a8020cf8ce4
Andrew <70240081+ABradbury823@users.noreply.github.com>
2025-04-16 01:05:15 -0400

    Refactor Authenticator component to integrate validation functions and improve error handling for sign-in and sign-up submissions

--- client/src/Authenticator/Authenticator.tsx
+++ client/src/Authenticator/Authenticator.tsx
@@ -1,15 +1,78 @@
-import { useEffect, useState } from "react"
-import { Button, Card, CardHeader, Nav, NavItem, TabContent, TabPane } from "reactstrap"
-import NavTab from "../NavTab/NavTab";
-import AuthForm from "./AuthForm/AuthForm";
-import { handleSignIn, handleSignOut, handleSignUp } from "../services/auth";
-import { getErrorMessage } from "../services/util";
+import { useEffect, useState } from 'react'
+import { Button, Card, CardHeader, Nav, NavItem, TabContent, TabPane } from 'reactstrap'
+import NavTab from '../NavTab/NavTab';
+import AuthForm from './AuthForm/AuthForm';
+import { handleSignIn, handleSignOut, handleSignUp, validateSignIn, validateSignUp } from '../services/auth';
+import { printDebugError } from '../services/util';
 
+/**
+ * Props for the Authenticator component.
+ * @param services - An object containing service functions for authentication.
+ * @param children - The child components to render when authenticated.
+ */
 interface AuthenticatorProps {
+  /**
+   * An object containing service functions for authentication.
+   * - handleSignOut: Function to handle sign-out.
+   * - handleSignIn: Function to handle sign-in.
+   * - handleSignUp: Function to handle sign-up.
+   * - onError: Function to handle errors.
+   * - validateSignIn: Function to validate sign-in data.
+   * - validateSignUp: Function to validate sign-up data.
+   */
+  services?: {
+    /**
+     * Function to handle sign-out.
+     * @returns Promise<void>
+     */
+    handleSignOut?: () => Promise<void>;
+    /**
+     * Function to handle sign-in.
+     * @param data - The form data from the sign-in form.
+     * @returns Promise<void>
+     */
+    handleSignIn?: (data?: FormData) => Promise<void>;
+    /**
+     * Function to handle sign-up.
+     * @param data - The form data from the sign-up form.
+     * @returns Promise<void>
+     */
+    handleSignUp?: (data?: FormData) => Promise<void>;
+    /**
+     * Function to handle errors.
+     * @param error - The error object.
+     */
+    onError?: (error: unknown) => void;
+    /**
+     * Function to validate sign-in and sign-up data.
+     * @param data - The form data to validate.
+     * @returns An object with field names as keys and error messages as values.
+     */
+    validateSignIn?: (data: FormData) => Record<string, string>;
+    /**
+     * Function to validate sign-up data.
+     * @param data - The form data to validate.
+     * @returns An object with field names as keys and error messages as values.
+     */
+    validateSignUp?: (data: FormData) => Record<string, string>;
+  };
+  /**
+   * The child components to render when authenticated.
+   */
   children?: React.ReactNode;
 }
 
-export default function Authenticator({ children }: AuthenticatorProps) {
+export default function Authenticator({ 
+  services = { 
+    handleSignOut, 
+    handleSignIn, 
+    handleSignUp, 
+    onError: printDebugError,
+    validateSignIn,
+    validateSignUp
+  },
+  children 
+}: AuthenticatorProps) {
   const [activeTab, setActiveTab] = useState(1);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -18,8 +81,8 @@ export default function Authenticator({ children }: AuthenticatorProps) {
    * Authentication determines how the UI is rendered.
    */
   useEffect(() => {
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
-    if(isAuthenticated === "true") {
+    const isAuthenticated = localStorage.getItem('isAuthenticated');
+    if(isAuthenticated === 'true') {
       setIsAuthenticated(true);
     }
   }, []);
@@ -36,11 +99,40 @@ export default function Authenticator({ children }: AuthenticatorProps) {
   }
 
   /**
+   * Handles sign-in form submission.
+   * @param formData The form data from the sign-in form.
+   */
+  const handleSignInSubmit = (formData?: FormData) => {
+    services.handleSignIn?.(formData)
+      .then(handleSuccessfulSignIn)
+      .catch(services.onError);
+  };
+  
+  /**
+   * Handles sign-up form submission.
+   * @param formData The form data from the sign-up form.
+   */
+  const handleSignUpSubmit = (formData?: FormData) => {
+    services.handleSignUp?.(formData)
+      .then(handleSuccessfulSignIn)
+      .catch(services.onError);
+  };
+
+  /**
+   * Handles sign-out form submission.
+   */
+  const handleSignOutSubmit = () => { 
+    services.handleSignOut?.()
+      .then(handleSuccessfulSignOut)
+      .catch(services.onError);
+  }
+
+  /**
    * Handles successful sign-in by updating the authentication state and storing it in local storage.
    */
   const handleSuccessfulSignIn = () => {
     setIsAuthenticated(true);
-    localStorage.setItem("isAuthenticated", "true");
+    localStorage.setItem('isAuthenticated', 'true');
   }
 
   /**
@@ -48,39 +140,32 @@ export default function Authenticator({ children }: AuthenticatorProps) {
    */
   const handleSuccessfulSignOut = () => {
     setIsAuthenticated(false);
-    localStorage.removeItem("isAuthenticated");
+    localStorage.removeItem('isAuthenticated');
   }
 
   // render content based on authentication status
   // if user is authenticated, show sign out button and children components
   if (isAuthenticated) {
     return (
-      <div className="d-flex flex-column align-items-center justify-content-center vh-100 vw-100">
+      <div className='d-flex flex-column align-items-center justify-content-center vh-100 vw-100'>
         <Nav className='navbar w-100 p-4 bg-secondary'>
-          <NavItem className="ms-auto">
-            <Button onClick={() =>
-              handleSignOut()
-              .then(handleSuccessfulSignOut)
-              .catch((error) => {
-                const message = getErrorMessage(error);
-                console.error(message);
-                alert(message);
-              })}
-              color="secondary">
+          <NavItem className='ms-auto'>
+            <Button onClick={handleSignOutSubmit}
+              color='secondary'>
               Sign Out
             </Button>
           </NavItem>
         </Nav>
-        <div className="flex-grow-1 d-flex flex-column">{ children } </div>
+        <div className='flex-grow-1 d-flex flex-column'>{ children } </div>
       </div>
     );
   }
 
   // if user is not authenticated, show sign in and sign up forms
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 vw-100">
-    <Card className="w-50">
-      <CardHeader className="p-2 m-2" tag='h2'>Welcome to MUPP</CardHeader>
+    <div className='d-flex justify-content-center align-items-center vh-100 vw-100'>
+    <Card className='w-50'>
+      <CardHeader className='p-2 m-2' tag='h2'>Welcome to MUPP</CardHeader>
       
       <Nav justified tabs>
         <NavTab
@@ -98,28 +183,21 @@ export default function Authenticator({ children }: AuthenticatorProps) {
         <TabPane tabId={1}>
           {/* Sign In Form */}
           <AuthForm
-            heading="Welcome Back"
-            submitLabel="Sign In"
-            onSubmit={(formData) => {
-              handleSignIn(formData)
-              .then(handleSuccessfulSignIn)
-              .catch((error) => {
-                const message = getErrorMessage(error);
-                console.error(message);
-                alert(message);
-              });
-            }}
+            heading='Welcome Back'
+            submitLabel='Sign In'
+            onSubmit={handleSignInSubmit}
+            validate={services.validateSignIn}
             formFields={
               [
                 {
-                  name: "sign-in-email",
-                  label: "Email",
+                  name: 'sign-in-email',
+                  label: 'Email',
                   type: 'email',
                   required: true
                 },
                 {
-                  name: "sign-in-password",
-                  label: "Password",
+                  name: 'sign-in-password',
+                  label: 'Password',
                   type: 'password',
                   required: true
                 }
@@ -130,51 +208,32 @@ export default function Authenticator({ children }: AuthenticatorProps) {
         <TabPane tabId={2}>
           {/* Create Account Form */}
           <AuthForm
-            heading="Create an Account"
-            submitLabel="Create Account"
-            onSubmit={(formData) => {
-              handleSignUp(formData)
-              .then(handleSuccessfulSignIn)
-              .catch((error) => {
-                const message = getErrorMessage(error);
-                console.error(message);
-                alert(message);
-              })
-            }}
-            validate={(data) => {
-              const errors: Record<string, string> = {};
-          
-              const password = data.get("sign-up-password")?.toString();
-              const confirmPassword = data.get("sign-up-confirm-password")?.toString();
-          
-              if (password !== confirmPassword) {
-                errors["sign-up-confirm-password"] = "Passwords do not match.";
-              }
-          
-              return errors;
-            }}
+            heading='Create an Account'
+            submitLabel='Create Account'
+            onSubmit={handleSignUpSubmit}
+            validate={services.validateSignUp}
             formFields={
               [
                 {
-                  name: "sign-up-username",
-                  label: "Username",
+                  name: 'sign-up-username',
+                  label: 'Username',
                   required: true
                 },
                 {
-                  name: "sign-up-email",
-                  label: "Email",
+                  name: 'sign-up-email',
+                  label: 'Email',
                   type: 'email',
                   required: true
                 },
                 {
-                  name: "sign-up-password",
-                  label: "Password",
+                  name: 'sign-up-password',
+                  label: 'Password',
                   type: 'password',
                   required: true
                 },
                 {
-                  name: "sign-up-confirm-password",
-                  label: "Confirm Password",
+                  name: 'sign-up-confirm-password',
+                  label: 'Confirm Password',
                   type: 'password',
                   required: true
                 }
