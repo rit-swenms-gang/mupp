4bc7b1afff15e0bf2561560eeaac704e98eb3ffd
Andrew <70240081+ABradbury823@users.noreply.github.com>
2025-04-17 14:47:41 -0400

    add form builder

---
+++ client/src/forms/builder.ts
@@ -0,0 +1,12 @@
+import { createBuilder } from "@coltorapps/builder";
+
+import { textFieldEntity } from "./entities";
+
+// Think of builders as collections of supported entities. 
+// For example, you can have a form builder that allows adding text and select fields to a form, 
+// but also another landing page builder that allows adding hero sections and feature sections to a landing page. 
+// For now, we're going to focus solely on the form builder.
+
+export const formBuilder = createBuilder({
+  entities: [textFieldEntity],
+});
