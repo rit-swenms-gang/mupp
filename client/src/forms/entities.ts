3880506969919a600c3bbf2723bfab093442ab8c
Andrew <70240081+ABradbury823@users.noreply.github.com>
2025-04-20 23:41:16 -0400

    number scale entity

--- client/src/forms/entities.ts
+++ client/src/forms/entities.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 import { createEntity } from '@coltorapps/builder';
 
-import { labelAttribute, requiredAttribute } from './attributes';
+import { labelAttribute, maxNumberAttribute, minNumberAttribute, requiredAttribute } from './attributes';
 
 // Think of entities with attributes as components with props. 
 // For example, you can define a text field entity, and users can later add multiple instances of text fields to a form.
@@ -26,11 +26,19 @@ export const textFieldEntity = createEntity({
     
 });
 
+/**
+ * This entity represents a number scale in a form.
+ * It has a label attribute and can be used to create a number input field.
+ */
 export const numberScaleEntity = createEntity({
   name: 'numberScale',
-  attributes: [labelAttribute, requiredAttribute],
+  attributes: [labelAttribute, requiredAttribute, minNumberAttribute, maxNumberAttribute],
   validate: (value, context) => {
-    const schema = z.number().min(0).max(10);
+    const schema = z.number()
+      .int()
+      .nonnegative()
+      .min(context.entity.attributes.minNumber ?? 1)
+      .max(context.entity.attributes.maxNumber ?? 10);
 
     if(!context.entity.attributes.required) {
       return schema.optional().parse(value);
