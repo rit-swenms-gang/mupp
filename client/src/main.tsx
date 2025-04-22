a216d12339223ce8aa77ee52b4a73a17a0dd01ed
Andrew Bradbury <70240081+ABradbury823@users.noreply.github.com>
2025-04-21 23:18:19 -0400

    routing and forms (#47)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 import App from './App/App.tsx'
+import FormPage from './forms/FormPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/forms/:formId" element={<FormPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+  
 )
